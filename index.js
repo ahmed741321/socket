dbb0918be6c7c6e0a635f0f16969624aef317a77
Ahmet Asaad Hammoud <88238215+ahmed741321@users.noreply.github.com>
2024-08-23 01:11:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const sockjs = require('sockjs');
 
+// إعداد التطبيق
 const app = express();
 const port = 3000;
 
@@ -14,33 +15,20 @@ app.get('/', (req, res) => {
 });
 
 // إعداد خادم SockJS
-const echo = sockjs.createServer({
+const sockjsOptions = {
     prefix: '/sockjs',
-    // لا حاجة لتحديد رقم المنفذ هنا لأننا سنقوم بإعداد السيرفر الرئيسي للسماح بـ SockJS على نفس المنفذ
-});
+};
 
-let clients = [];
+const echo = sockjs.createServer(sockjsOptions);
 
 echo.on('connection', function (conn) {
     console.log('Client connected');
-    clients.push(conn);
-
-    // إرسال إشعار لجميع العملاء عند اتصال عميل جديد
-    const joinMessage = JSON.stringify({
-        username: 'Server',
-        text: 'A new user has joined the chat.',
-    });
-
-    clients.forEach(client => {
-        if (client !== conn) {
-            client.write(joinMessage);
-        }
-    });
 
     conn.on('data', function (message) {
-        //console.log('Received:', message);
+        console.log('Received:', message);
+
         // إعادة إرسال الرسالة إلى جميع العملاء المتصلين
-        clients.forEach(client => {
+        echo.clients.forEach(function (client) {
             if (client !== conn) {
                 client.write(message);
             }
@@ -49,23 +37,11 @@ echo.on('connection', function (conn) {
 
     conn.on('close', function () {
         console.log('Client disconnected');
-        // إزالة العميل من قائمة العملاء
-        clients = clients.filter(client => client !== conn);
-
-        // إرسال إشعار لجميع العملاء عند مغادرة عميل
-        const leaveMessage = JSON.stringify({
-            username: 'Server',
-            text: 'A user has left the chat.',
-        });
-
-        clients.forEach(client => {
-            client.write(leaveMessage);
-        });
     });
 });
 
 const server = app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://your-domain.com:${port}`);
 });
 
 echo.installHandlers(server);
